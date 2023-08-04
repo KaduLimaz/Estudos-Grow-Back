a35b21561bcc42d5fa2f985f38bd3fc4c8013103
Carlos Eduardo Sabino de Lima <121118484+KaduLimaz@users.noreply.github.com>
2023-08-04 20:33:42 -0300

    Introdução Arrays
    atividades simples - finalizadas

---
+++ Atividade Pratica 07 - Arrays 1/atividade 02/01.js
@@ -0,0 +1,14 @@
+// 2. Criar um array e percorrê-lo utilizando o loop for of
+// a. Crie um array com o nome "numeros" e adicione 5 números de
+// sua escolha
+// b. Utilize o loop for of para percorrer o array e exibir a soma dos números na tela
+
+
+
+
+const numeros = [10,20,30,40,50];
+
+
+for( numero of numeros) {
+    console.log(numero);
+}
---
+++ Atividade Pratica 07 - Arrays 1/atividade 03/01.js
@@ -0,0 +1,34 @@
+// 3. Utilizar os métodos push, pop, unshift e shift para manipular um
+// array
+
+// de sua escolha
+
+
+
+// array
+// e. Utilize o método shift para remover a primeira fruta do array
+// f. Exiba o array resultante na tela
+
+// a. Crie um array com o nome "frutas" e adicione 3 tipos de frutas
+const frutas = ["uva","maçã","laranja"];
+
+// b. Utilize o método push para adicionar uma fruta no final do
+// array
+frutas.push("banana");
+
+console.log(frutas);
+
+// c. Utilize o método pop para remover a última fruta do array
+frutas.pop();
+
+console.log(frutas);
+
+// d. Utilize o método unshift para adicionar uma fruta no início do
+frutas.unshift("melancia");
+
+console.log(frutas);
+
+// e. Utilize o método shift para remover a primeira fruta do array
+
+frutas.shift()
+console.log(frutas);
